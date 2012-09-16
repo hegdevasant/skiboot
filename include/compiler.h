d366a87767bbf1fac4f55a487518032fbc534fb8
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2012-09-17 09:20:27 +1000

    Move compiler macros to compiler.h
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

---
+++ include/compiler.h
@@ -0,0 +1,24 @@
+#ifndef __COMPILER_H
+#define __COMPILER_H
+
+#ifndef __ASSEMBLY__
+
+#include <stddef.h>
+
+/* Macros for various compiler bits and pieces */
+#define __packed		__attribute__((packed))
+#define __align(x)		__attribute__((__aligned__(x)))
+#define __unused		__attribute__((unused))
+#define __section(x)		__attribute__((__section__(x)))
+
+#if 0 /* Provided by gcc stddef.h */
+#define offsetof(type,m)	__builtin_offsetof(type,m)
+#endif
+
+#endif /* __ASSEMBLY__ */
+
+/* Stringification macro */
+#define __tostr(x)	#x
+#define tostr(x)	__tostr(x)
+
+#endif /* __COMPILER_H */
--- include/skiboot.h
+++ include/skiboot.h
@@ -1,17 +1,8 @@
 #ifndef __SKIBOOT_H
 #define __SKIBOOT_H
 
-#include <stddef.h>
-
-/* Macros for various compiler bits and pieces */
-#define __packed		__attribute__((packed))
-#define __align(x)		__attribute__((__aligned__(x)))
-#define __unused		__attribute__((unused))
-#define __section(x)		__attribute__((__section__(x)))
-
-#if 0 /* Provided by gcc stddef.h */
-#define offsetof(type,m)	__builtin_offsetof(type,m)
-#endif
+#include <compiler.h>
+#include <stdint.h>
 
 /* Special ELF sections */
 #define __force_data		__section(".force.data")
