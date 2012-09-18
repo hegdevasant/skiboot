d3ab5f02c3a77069a3ed3694ca3920f76908719d
Rusty Russell <rusty@rustcorp.com.au>
2012-09-18 17:30:04 +0930

    libc: implement abort().
    Signed-off-by: Rusty Russell <rusty@au.ibm.com>

---
+++ libc/stdlib/abort.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+ * Copyright (c) 2004, 2008, 2012 IBM Corporation
+ * All rights reserved.
+ * This program and the accompanying materials
+ * are made available under the terms of the BSD License
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * Contributors:
+ *     IBM Corporation - initial implementation
+ *****************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void abort(void)
+{
+	fputs(stderr, "Aborting!\n");
+	for (;;);
+}
