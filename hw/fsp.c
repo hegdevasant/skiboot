bf39802947d0695645c1734ebd73330fd858e3c8
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2012-09-23 08:30:38 +1000

    Add some more tracing of inbound buffer requests
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- hw/fsp.c
+++ hw/fsp.c
@@ -431,6 +431,8 @@ static void  fsp_alloc_inbound(struct fsp_msg *msg)
 	len = (len + 0xfff) & ~0xfff;
 	fsp_inbound_off += len;
 	fsp_tce_map(tce_token, buf, len);
+	printf("FSP:  -> buffer at 0x%p, TCE: 0x%08x, alen: 0x%x\n",
+	       buf, tce_token, len);
 	act_len = len;
 
  reply:
