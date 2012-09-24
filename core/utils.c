6799149af06087cef19e6021ecf84a6a3fb868a0
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2012-09-24 15:24:45 +1000

    Improve abort()
    Avoid recursion and poll the FSP to allow incoming debug connections
    on the virtual serial.
    
    We could make that more robust by setting some kind of flag for the FSP
    driver to ignore all packets that aren't virtual serial.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- core/utils.c
+++ core/utils.c
@@ -1,15 +1,23 @@
 #include <skiboot.h>
 #include <lock.h>
+#include <fsp.h>
 
 void abort(void)
 {
+	static bool in_abort = false;
+
+	if (in_abort)
+		for (;;) ;
+	in_abort = true;
+
 	bust_locks = true;
 
 	op_display(OP_FATAL, OP_MOD_CORE, 0x6666);
 	
 	fputs(stderr, "Aborting!\n");
 	backtrace();
-	for (;;);
+	for (;;)
+		fsp_poll();
 }
 
 char tohex(uint8_t nibble)
