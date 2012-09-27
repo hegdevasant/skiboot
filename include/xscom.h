f803be184fd16df8b401d1e75c35c38d5318dcca
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2012-09-27 20:48:49 +1000

    xscom uses PIR from per-cpu data rather than SPR
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- include/xscom.h
+++ include/xscom.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <processor.h>
+#include <cpu.h>
 
 /*
  * Convert a PIR value to a Global Chip ID (insert Torrent bit)
@@ -33,12 +34,16 @@ extern int xscom_write(uint32_t gcid, uint32_t pcb_addr, uint64_t val);
 /* This chip SCOM access */
 static inline int xscom_readme(uint32_t pcb_addr, uint64_t *val)
 {
-	return xscom_read(PIR2GCID(mfspr(SPR_PIR)), pcb_addr, val);
+	uint32_t pir = this_cpu()->pir;
+
+	return xscom_read(PIR2GCID(pir), pcb_addr, val);
 }
 
 static inline int xscom_writeme(uint32_t pcb_addr, uint64_t val)
 {
-	return xscom_write(PIR2GCID(mfspr(SPR_PIR)), pcb_addr, val);
+	uint32_t pir = this_cpu()->pir;
+
+	return xscom_write(PIR2GCID(pir), pcb_addr, val);
 }
 
 extern void xscom_init(void);
