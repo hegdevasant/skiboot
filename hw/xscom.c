efa3889cff3e1d4e726d27bb2548f7dfec91aa38
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2012-09-27 21:57:28 +1000

    Improve XSCOM support
    No real error handling but at least handle blocked status
    properly and detect errors
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- hw/xscom.c
+++ hw/xscom.c
@@ -1,12 +1,16 @@
 #include <xscom.h>
 #include <io.h>
 #include <spira.h>
+#include <processor.h>
 
 /* XSCOM base address default */
 #define XSCOM_DEFAULT_BASE	0x00001A0000000000UL
-
 static uint64_t xscom_base = XSCOM_DEFAULT_BASE;
 
+/* Mask of bits to clear in HMER before an access */
+#define HMER_CLR_MASK	(~(SPR_HMER_XSCOM_FAIL | \
+			   SPR_HMER_XSCOM_DONE | \
+			   SPR_HMER_XSCOM_STATUS_MASK))
 
 static inline void *xscom_addr(uint32_t gcid, uint32_t pcb_addr)
 {
@@ -29,17 +33,89 @@ static void __xscom_write(uint32_t gcid, uint32_t pcb_addr, uint64_t val)
 	out_be64(xscom_addr(gcid, pcb_addr), val);
 }
 
+bool xscom_handle_error(uint64_t hmer, uint32_t gcid, uint32_t pcb_addr,
+			bool is_write)
+{
+	unsigned int stat = PPC_GETFIELD(SPR_HMER_XSCOM_STATUS, hmer);
+
+	/* XXX Figure out error codes from doc and error
+	 * recovery procedures
+	 */
+	switch(stat) {
+	/* XSCOM blocked, just retry */
+	case 1:
+		return true;
+	}
+
+	prerror("XSCOM: %s error, gcid: 0x%x pcb_addr: 0x%x stat: 0x%x\n",
+		is_write ? "write" : "read", gcid, pcb_addr, stat);
+
+	/* Non recovered ... just fail */
+	return false;
+}
+
+static uint64_t xscom_wait_done(void)
+{
+	uint64_t hmer;
+
+	do
+		hmer = mfspr(SPR_HMER);
+	while(!(hmer & SPR_HMER_XSCOM_DONE));
+
+	return hmer;
+}
+
 int xscom_read(uint32_t gcid, uint32_t pcb_addr, uint64_t *val)
 {
-	/* XXX Implement error handling and recovery !!! */
-	*val = __xscom_read(gcid, pcb_addr);
+	uint64_t hmer;
+
+	for (;;) {
+		/* Clear status bits in HMER (HMER is special
+		 * writing to it *ands* bits
+		 */
+		mtspr(SPR_HMER, HMER_CLR_MASK);
+
+		/* Read value from SCOM */
+		*val = __xscom_read(gcid, pcb_addr);
+
+		/* Wait for done bit */
+		hmer = xscom_wait_done();
+
+		/* Check for error */
+		if (!(hmer & SPR_HMER_XSCOM_FAIL))
+			break;
+
+		/* Handle error and eventually retry */
+		if (!xscom_handle_error(hmer, gcid, pcb_addr, false))
+			return -1;
+	}
 	return 0;
 }
 
 int xscom_write(uint32_t gcid, uint32_t pcb_addr, uint64_t val)
 {
-	/* XXX Implement error handling and recovery !!! */
-	__xscom_write(gcid, pcb_addr, val);
+	uint64_t hmer;
+
+	for (;;) {
+		/* Clear status bits in HMER (HMER is special
+		 * writing to it *ands* bits
+		 */
+		mtspr(SPR_HMER, HMER_CLR_MASK);
+
+		/* Write value to SCOM */
+		__xscom_write(gcid, pcb_addr, val);
+
+		/* Wait for done bit */
+		hmer = xscom_wait_done();
+
+		/* Check for error */
+		if (!(hmer & SPR_HMER_XSCOM_FAIL))
+			break;
+
+		/* Handle error and eventually retry */
+		if (!xscom_handle_error(hmer, gcid, pcb_addr, true))
+			return -1;
+	}
 	return 0;
 }
 
