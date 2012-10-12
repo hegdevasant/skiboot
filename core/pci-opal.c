4de8c3296ec7e80cab7b7d802867522dd7b679a4
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2012-10-12 15:37:57 +1100

    Add a generic PCI layer
    Defines a generic PHB object with function pointers for low
    level operations. Provides the OPAL API wrappers, initial
    bus walking and device-tree generation.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

---
+++ core/pci-opal.c
@@ -0,0 +1,458 @@
+#include <skiboot.h>
+#include <pci.h>
+#include <pci-cfg.h>
+#include <time.h>
+#include <lock.h>
+
+static int64_t opal_pci_config_read_byte(uint64_t phb_id,
+					 uint64_t bus_dev_func,
+					 uint64_t offset, uint8_t *data)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->cfg_read8(phb, bus_dev_func, offset, data);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_CONFIG_READ_BYTE, opal_pci_config_read_byte);
+
+static int64_t opal_pci_config_read_half_word(uint64_t phb_id,
+					      uint64_t bus_dev_func,
+					      uint64_t offset, uint16_t *data)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->cfg_read16(phb, bus_dev_func, offset, data);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_CONFIG_READ_HALF_WORD, opal_pci_config_read_half_word);
+
+static int64_t opal_pci_config_read_word(uint64_t phb_id,
+					 uint64_t bus_dev_func,
+					 uint64_t offset, uint32_t *data)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->cfg_read32(phb, bus_dev_func, offset, data);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_CONFIG_READ_WORD, opal_pci_config_read_word);
+
+static int64_t opal_pci_config_write_byte(uint64_t phb_id,
+					  uint64_t bus_dev_func,
+					  uint64_t offset, uint8_t data)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->cfg_write8(phb, bus_dev_func, offset, data);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_CONFIG_WRITE_BYTE, opal_pci_config_write_byte);
+
+static int64_t opal_pci_config_write_half_word(uint64_t phb_id,
+					       uint64_t bus_dev_func,
+					       uint64_t offset, uint16_t data)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->cfg_write16(phb, bus_dev_func, offset, data);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_CONFIG_WRITE_HALF_WORD, opal_pci_config_write_half_word);
+
+static int64_t opal_pci_config_write_word(uint64_t phb_id,
+					  uint64_t bus_dev_func,
+					  uint64_t offset, uint32_t data)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->cfg_write32(phb, bus_dev_func, offset, data);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_CONFIG_WRITE_WORD, opal_pci_config_write_word);
+
+static int64_t opal_pci_eeh_freeze_status(uint64_t phb_id, uint64_t pe_number,
+					  uint8_t *freeze_state,
+					  uint16_t *pci_error_type,
+					  uint64_t *phb_status)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->eeh_freeze_status(phb, pe_number, freeze_state,
+					 pci_error_type, phb_status);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_EEH_FREEZE_STATUS, opal_pci_eeh_freeze_status);
+
+static int64_t opal_pci_eeh_freeze_clear(uint64_t phb_id, uint64_t pe_number,
+					 uint64_t eeh_action_token)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->eeh_freeze_clear(phb, pe_number, eeh_action_token);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_EEH_FREEZE_CLEAR, opal_pci_eeh_freeze_clear);
+
+static int64_t opal_pci_phb_mmio_enable(uint64_t phb_id, uint16_t window_type,
+					uint16_t window_num, uint16_t enable)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->phb_mmio_enable(phb, window_type, window_num, enable);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_PHB_MMIO_ENABLE, opal_pci_phb_mmio_enable);
+
+static int64_t opal_pci_set_phb_mem_window(uint64_t phb_id,
+					   uint16_t window_type,
+					   uint16_t window_num,
+					   uint64_t starting_real_address,
+					   uint64_t starting_pci_address,
+					   uint16_t segment_size)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->set_phb_mem_window(phb, window_type, window_num,
+					  starting_real_address,
+					  starting_pci_address,
+					  segment_size);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_SET_PHB_MEM_WINDOW, opal_pci_set_phb_mem_window);
+
+static int64_t opal_pci_map_pe_mmio_window(uint64_t phb_id, uint16_t pe_number,
+					   uint16_t window_type,
+					   uint16_t window_num,
+					   uint16_t segment_num)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->map_pe_mmio_window(phb, pe_number, window_type,
+					  window_num, segment_num);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_MAP_PE_MMIO_WINDOW, opal_pci_map_pe_mmio_window);
+
+static int64_t opal_pci_set_phb_table_memory(uint64_t phb_id __unused,
+					     uint64_t rtt_addr __unused,
+					     uint64_t ivt_addr __unused,
+					     uint64_t ivt_len __unused,
+					     uint64_t rej_array_addr __unused,
+					     uint64_t peltv_addr __unused)
+{
+	/* IODA2 (P8) stuff, TODO */
+	return OPAL_UNSUPPORTED;
+}
+opal_call(OPAL_PCI_SET_PHB_TABLE_MEMORY, opal_pci_set_phb_table_memory);
+
+static int64_t opal_pci_set_pe(uint64_t phb_id, uint64_t pe_number,
+			       uint64_t bus_dev_func, uint8_t bus_compare,
+			       uint8_t dev_compare, uint8_t func_compare,
+			       uint8_t pe_action)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->set_pe(phb, pe_number, bus_dev_func, bus_compare,
+			      dev_compare, func_compare, pe_action);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_SET_PE, opal_pci_set_pe);
+
+static int64_t opal_pci_set_peltv(uint64_t phb_id, uint32_t parent_pe,
+				  uint32_t child_pe, uint8_t state)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->set_peltv(phb, parent_pe, child_pe, state);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_SET_PELTV, opal_pci_set_peltv);
+
+static int64_t opal_pci_set_mve(uint64_t phb_id, uint32_t mve_number,
+				uint32_t pe_number)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->set_mve(phb, mve_number, pe_number);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_SET_MVE, opal_pci_set_mve);
+
+static int64_t opal_pci_set_mve_enable(uint64_t phb_id, uint32_t mve_number,
+				       uint32_t state)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->set_mve_enable(phb, mve_number, state);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_SET_MVE_ENABLE, opal_pci_set_mve_enable);
+
+static int64_t opal_pci_get_xive_reissue(uint64_t phb_id __unused,
+					 uint32_t xive_number __unused,
+					 uint8_t *p_bit __unused,
+					 uint8_t *q_bit __unused)
+{
+	/* IODA2 (P8) stuff, TODO */
+	return OPAL_UNSUPPORTED;
+}
+opal_call(OPAL_PCI_GET_XIVE_REISSUE, opal_pci_get_xive_reissue);
+
+static int64_t opal_pci_set_xive_reissue(uint64_t phb_id __unused,
+					 uint32_t xive_number __unused,
+					 uint8_t p_bit __unused,
+					 uint8_t q_bit __unused)
+{
+	/* IODA2 (P8) stuff, TODO */
+	return OPAL_UNSUPPORTED;
+}
+opal_call(OPAL_PCI_SET_XIVE_REISSUE, opal_pci_set_xive_reissue);
+
+static int64_t opal_pci_set_xive_pe(uint64_t phb_id, uint32_t pe_number,
+				    uint32_t xive_num)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->set_xive_pe(phb, pe_number, xive_num);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_SET_XIVE_PE, opal_pci_set_xive_pe);
+
+static int64_t opal_get_xive_source(uint64_t phb_id, uint32_t xive_num,
+				    int32_t *interrupt_source_number)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->get_xive_source(phb, xive_num, interrupt_source_number);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_GET_XIVE_SOURCE, opal_get_xive_source);
+
+static int64_t opal_get_msi_32(uint64_t phb_id, uint32_t mve_number,
+			       uint32_t xive_num, uint8_t msi_range,
+			       uint32_t *msi_address, uint32_t *message_data)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->get_msi_32(phb, mve_number, xive_num, msi_range,
+				  msi_address, message_data);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_GET_MSI_32, opal_get_msi_32);
+
+static int64_t opal_get_msi_64(uint64_t phb_id, uint32_t mve_number,
+			       uint32_t xive_num, uint8_t msi_range,
+			       uint64_t *msi_address, uint32_t *message_data)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->get_msi_64(phb, mve_number, xive_num, msi_range,
+				  msi_address, message_data);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_GET_MSI_64, opal_get_msi_64);
+
+static int64_t opal_pci_map_pe_dma_window(uint64_t phb_id, uint16_t pe_number,
+					  uint16_t window_id,
+					  uint16_t tce_levels,
+					  uint64_t tce_table_addr,
+					  uint64_t tce_table_size,
+					  uint64_t tce_page_size)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->map_pe_dma_window(phb, pe_number, window_id,
+					 tce_levels, tce_table_addr,
+					 tce_table_size, tce_page_size);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_MAP_PE_DMA_WINDOW, opal_pci_map_pe_dma_window);
+
+static int64_t opal_pci_map_pe_dma_window_real(uint64_t phb_id,
+					       uint16_t pe_number,
+					       uint16_t window_id,
+					       uint64_t pci_start_addr,
+					       uint64_t pci_mem_size)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+	rc = phb->ops->map_pe_dma_window_real(phb, pe_number, window_id,
+					      pci_start_addr, pci_mem_size);
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_MAP_PE_DMA_WINDOW_REAL, opal_pci_map_pe_dma_window_real);
+
+static int64_t opal_pci_reset(uint64_t phb_id, uint8_t reset_scope,
+			      uint8_t assert_state)
+{
+	struct phb *phb = pci_get_phb(phb_id);
+	int64_t rc = OPAL_SUCCESS;
+
+	if (!phb)
+		return OPAL_PARAMETER;
+	phb->ops->lock(phb);
+
+	/* We currently only handle IODA reset */
+	switch(reset_scope) {
+	case OPAL_PCI_IODA_TABLE_RESET:
+		if (assert_state != OPAL_ASSERT_RESET)
+			break;
+		phb->ops->ioda_reset(phb);
+		break;
+	default:
+		rc = OPAL_UNSUPPORTED;
+	}
+	phb->ops->unlock(phb);
+	pci_put_phb(phb);
+
+	return rc;
+}
+opal_call(OPAL_PCI_RESET, opal_pci_reset);
+
---
+++ include/pci.h
@@ -0,0 +1,320 @@
+#ifndef __PCI_H
+#define __PCI_H
+
+#include <opal.h>
+#include <ccan/list/list.h>
+
+/*
+ * While this might not be necessary in the long run, the existing
+ * Linux kernels expect us to provide a device-tree that contains
+ * a representation of all PCI devices below the host bridge. Thus
+ * we need to perform a bus scan. We don't need to assign MMIO/IO
+ * resources, but we do need to assign bus numbers in a way that
+ * is going to be compatible with the HW constraints for PE filtering
+ * that is naturally aligned power of twos for ranges below a bridge.
+ *
+ * Thus the structure pci_device is used for the tracking of the
+ * detected devices and the later generation of the device-tree.
+ *
+ * We do not keep a separate structure for a bus, however a device
+ * can have children in which case a device is a bridge.
+ *
+ * Because this is likely to change, we avoid putting too much
+ * information in that structure nor relying on it for anything
+ * else but the construction of the flat device-tree.
+ */
+struct pci_device {
+	uint16_t		bdfn;
+	bool			is_bridge;
+	bool			is_pcie;
+	bool			is_multifunction;
+	uint8_t			dev_type; /* PCIE */
+	uint32_t		scan_map;
+	struct list_head	children;
+	struct list_node	link;
+};
+
+/*
+ * When generating the device-tree, we need to keep track of
+ * the LSI mapping & swizzle it. This state structure is
+ * passed by the PHB to pci_add_nodes() and will be used
+ * internally.
+ *
+ * We assume that the interrupt parent (PIC) #address-cells
+ * is 0 and #interrupt-cells has a max value of 2.
+ */
+struct pci_lsi_state {
+#define MAX_INT_SIZE	2
+	uint32_t int_size;			/* #cells */
+	uint32_t int_val[4][MAX_INT_SIZE];	/* INTA...INTD */
+	uint32_t int_parent[4];	
+};
+
+/*
+ * NOTE: All PCI functions return negative OPAL error codes
+ *
+ * In addition, some functions may return a positive timeout
+ * value or some other state information, see the description	
+ * of individual functions. If nothing is specified, it's
+ * just an error code or 0 (success).
+ *
+ * Functions that operate asynchronously will return a positive
+ * delay value and will require the ->poll() op to be called after
+ * that delay. ->poll() will then return success, a negative error
+ * code, or another delay.
+ *
+ * Note: If an asynchronous function returns 0, it has completed
+ * successfully and does not require a call to ->poll(). Similarily
+ * if ->poll() is called while no operation is in progress, it will
+ * simply return 0 (success)
+ *
+ * Note that all functions except ->lock() itself assume that the
+ * caller is holding the PHB lock.
+ *
+ * TODO: Add more interfaces to control things like link width
+ *       reduction for power savings etc...
+ */
+
+struct phb;
+
+struct phb_ops {
+	/*
+	 * Locking. This is called around OPAL accesses
+	 */
+	void (*lock)(struct phb *phb);
+	void (*unlock)(struct phb *phb);
+
+	/*
+	 * Config space ops
+	 */
+	int64_t (*cfg_read8)(struct phb *phb, uint32_t bdfn,
+			     uint32_t offset, uint8_t *data);
+	int64_t (*cfg_read16)(struct phb *phb, uint32_t bdfn,
+			      uint32_t offset, uint16_t *data);
+	int64_t (*cfg_read32)(struct phb *phb, uint32_t bdfn,
+			      uint32_t offset, uint32_t *data);
+	int64_t (*cfg_write8)(struct phb *phb, uint32_t bdfn,
+			      uint32_t offset, uint8_t data);
+	int64_t (*cfg_write16)(struct phb *phb, uint32_t bdfn,
+			       uint32_t offset, uint16_t data);
+	int64_t (*cfg_write32)(struct phb *phb, uint32_t bdfn,
+			       uint32_t offset, uint32_t data);
+
+	/*
+	 * Bus number selection. See pci_scan() for a description
+	 */
+	uint8_t (*choose_bus)(struct phb *phb, struct pci_device *bridge,
+			      uint8_t candidate, uint8_t *max_bus,
+			      bool *use_max);
+	/*
+	 * EEH methods
+	 *
+	 * The various arguments are identical to the corresponding
+	 * OPAL functions
+	 */
+	int64_t (*eeh_freeze_status)(struct phb *phb, uint64_t pe_number,
+				     uint8_t *freeze_state,
+				     uint16_t *pci_error_type,
+				     uint64_t *phb_status);
+	int64_t (*eeh_freeze_clear)(struct phb *phb, uint64_t pe_number,
+				    uint64_t eeh_action_token);
+
+	/*
+	 * Other IODA methods
+	 *
+	 * The various arguments are identical to the corresponding
+	 * OPAL functions
+	 */
+	int64_t (*phb_mmio_enable)(struct phb *phb, uint16_t window_type,
+				   uint16_t window_num, uint16_t enable);
+
+	int64_t (*set_phb_mem_window)(struct phb *phb, uint16_t window_type,
+				      uint16_t window_num,
+				      uint64_t starting_real_address,
+				      uint64_t starting_pci_address,
+				      uint16_t segment_size);
+
+	int64_t (*map_pe_mmio_window)(struct phb *phb, uint16_t pe_number,
+				      uint16_t window_type, uint16_t window_num,
+				      uint16_t segment_num);
+
+	int64_t (*set_pe)(struct phb *phb, uint64_t pe_number,
+			  uint64_t bus_dev_func, uint8_t bus_compare,
+			  uint8_t dev_compare, uint8_t func_compare,
+			  uint8_t pe_action);
+
+	int64_t (*set_peltv)(struct phb *phb, uint32_t parent_pe,
+			     uint32_t child_pe, uint8_t state);
+
+	int64_t (*map_pe_dma_window)(struct phb *phb, uint16_t pe_number,
+				     uint16_t window_id, uint16_t tce_levels,
+				     uint64_t tce_table_addr,
+				     uint64_t tce_table_size,
+				     uint64_t tce_page_size);
+
+	int64_t (*map_pe_dma_window_real)(struct phb *phb, uint16_t pe_number,
+					  uint16_t dma_window_number,
+					  uint64_t pci_start_addr,
+					  uint64_t pci_mem_size);
+
+	int64_t (*set_mve)(struct phb *phb, uint32_t mve_number,
+			   uint32_t pe_number);
+
+	int64_t (*set_mve_enable)(struct phb *phb, uint32_t mve_number,
+				  uint32_t state);
+
+	int64_t (*set_xive_pe)(struct phb *phb, uint32_t pe_number,
+			       uint32_t xive_num);
+
+	int64_t (*get_xive_source)(struct phb *phb, uint32_t xive_num,
+				   int32_t *interrupt_source_number);
+
+	int64_t (*get_msi_32)(struct phb *phb, uint32_t mve_number,
+			      uint32_t xive_num, uint8_t msi_range,
+			      uint32_t *msi_address, uint32_t *message_data);
+
+	int64_t (*get_msi_64)(struct phb *phb, uint32_t mve_number,
+			      uint32_t xive_num, uint8_t msi_range,
+			      uint64_t *msi_address, uint32_t *message_data);
+
+	int64_t (*ioda_reset)(struct phb *phb);
+
+
+	/*
+	 * Slot control
+	 */
+
+	/* presence_detect - Check for a present device
+	 *
+	 * Immediate return of:
+	 *
+	 * OPAL_SHPC_DEV_NOT_PRESENT = 0,
+	 * OPAL_SHPC_DEV_PRESENT = 1
+	 *
+	 * or a negative OPAL error code
+	 */
+	int64_t (*presence_detect)(struct phb *phb);
+
+	/* link_state - Check link state
+	 *
+	 * Immediate return of:
+	 *
+	 * OPAL_SHPC_LINK_DOWN = 0,
+	 * OPAL_SHPC_LINK_UP_x1 = 1,
+	 * OPAL_SHPC_LINK_UP_x2 = 2,
+	 * OPAL_SHPC_LINK_UP_x4 = 4,
+	 * OPAL_SHPC_LINK_UP_x8 = 8,
+	 * OPAL_SHPC_LINK_UP_x16 = 16,
+	 * OPAL_SHPC_LINK_UP_x32 = 32
+	 *
+	 * or a negative OPAL error code
+	 */
+	int64_t (*link_state)(struct phb *phb);
+
+	/* power_state - Check slot power state
+	 *
+	 * Immediate return of:
+	 *
+	 * OPAL_SLOT_POWER_OFF = 0,
+	 * OPAL_SLOT_POWER_ON = 1,
+	 *
+	 * or a negative OPAL error code
+	 */
+	int64_t (*power_state)(struct phb *phb);
+
+	/* slot_power_off - Start slot power off sequence
+	 *
+	 * Asynchronous function, returns a positive delay
+	 * or a negative error code
+	 */
+	int64_t (*slot_power_off)(struct phb *phb);
+
+	/* slot_power_on - Start slot power on sequence
+	 *
+	 * Asynchronous function, returns a positive delay
+	 * or a negative error code.
+	 */
+	int64_t (*slot_power_on)(struct phb *phb);
+
+	/* hot_reset - Hot Reset sequence
+	 *
+	 * Asynchronous function, returns a positive delay
+	 * or a negative error code.
+	 *
+	 * Error codes (on completion, possibly via poll)
+	 *
+	 *  - OPAL_BUSY     : Another async op in progress
+	 *  - OPAL_HARDWARE : Any error including timeout waiting for link
+	 *  - OPAL_CLOSED   : No device detected in the slot
+	 */
+	int64_t (*hot_reset)(struct phb *phb);
+
+	/* poll - Poll and advance asynchronous operations
+	 *
+	 * Returns a positive delay, 0 for success or a
+	 * negative OPAL error code
+	 */
+	int64_t (*poll)(struct phb *phb);
+};
+
+struct phb {
+	int			opal_id;
+	uint8_t			scan_map;
+	struct list_head	devices;
+	const struct phb_ops	*ops;
+};
+
+/* Config space ops wrappers */
+static inline int64_t pci_cfg_read8(struct phb *phb, uint32_t bdfn,
+				    uint32_t offset, uint8_t *data)
+{
+	return phb->ops->cfg_read8(phb, bdfn, offset, data);
+}
+
+static inline int64_t pci_cfg_read16(struct phb *phb, uint32_t bdfn,
+				     uint32_t offset, uint16_t *data)
+{
+	return phb->ops->cfg_read16(phb, bdfn, offset, data);
+}
+
+static inline int64_t pci_cfg_read32(struct phb *phb, uint32_t bdfn,
+				     uint32_t offset, uint32_t *data)
+{
+	return phb->ops->cfg_read32(phb, bdfn, offset, data);
+}
+
+static inline int64_t pci_cfg_write8(struct phb *phb, uint32_t bdfn,
+				     uint32_t offset, uint8_t data)
+{
+	return phb->ops->cfg_write8(phb, bdfn, offset, data);
+}
+
+static inline int64_t pci_cfg_write16(struct phb *phb, uint32_t bdfn,
+				      uint32_t offset, uint16_t data)
+{
+	return phb->ops->cfg_write16(phb, bdfn, offset, data);
+}
+
+static inline int64_t pci_cfg_write32(struct phb *phb, uint32_t bdfn,
+				      uint32_t offset, uint32_t data)
+{
+	return phb->ops->cfg_write32(phb, bdfn, offset, data);
+}
+
+/* Utilities */
+extern int64_t pci_find_cap(struct phb *phb, uint16_t bdfn, uint8_t cap);
+extern int64_t pci_find_ecap(struct phb *phb, uint16_t bdfn, uint16_t cap,
+			     uint8_t *version);
+
+/* Manage PHBs */
+extern int64_t pci_register_phb(struct phb *phb);
+extern int64_t pci_unregister_phb(struct phb *phb);
+extern struct phb *pci_get_phb(uint64_t phb_id);
+static inline void pci_put_phb(struct phb *phb __unused) { }
+
+/* Device tree */
+extern void pci_add_nodes(struct phb *phb, struct pci_lsi_state *lstate);
+/* Initialize all PCI slots */
+extern void pci_init_slots(void);
+
+#endif /* __PCI_H */
