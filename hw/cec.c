c85241c8c6f87ab1401ddc63e4fd500bad60a327
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2012-10-16 13:22:18 +1100

    Add OPAL calls for IO Hub get_diag_data() and set_tce_memory()
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- hw/cec.c
+++ hw/cec.c
@@ -201,3 +201,34 @@ void cec_init(void)
 	pci_init_slots();
 }
 
+static int64_t opal_pci_set_hub_tce_memory(uint64_t hub_id,
+					   uint64_t tce_mem_addr,
+					   uint64_t tce_mem_size)
+{
+	struct io_hub *hub = cec_get_hub_by_id(hub_id);
+
+	if (!hub)
+		return OPAL_PARAMETER;
+
+	if (!hub->ops->set_tce_mem)
+		return OPAL_UNSUPPORTED;
+
+	return hub->ops->set_tce_mem(hub, tce_mem_addr, tce_mem_size);
+}
+opal_call(OPAL_PCI_SET_HUB_TCE_MEMORY, opal_pci_set_hub_tce_memory);
+
+static int64_t opal_pci_get_hub_diag_data(uint64_t hub_id,
+					  void *diag_buffer,
+					  uint64_t diag_buffer_len)
+{
+	struct io_hub *hub = cec_get_hub_by_id(hub_id);
+
+	if (!hub)
+		return OPAL_PARAMETER;
+
+	if (!hub->ops->get_diag_data)
+		return OPAL_UNSUPPORTED;
+
+	return hub->ops->get_diag_data(hub, diag_buffer, diag_buffer_len);
+}
+opal_call(OPAL_PCI_GET_HUB_DIAG_DATA, opal_pci_get_hub_diag_data);
