1297e11d1c1e5257dcc13aeaf764bdb4ba34807d
Rusty Russell <rusty@rustcorp.com.au>
2012-11-02 17:32:04 +1030

    asm_offsets.h: use our own script to generate it.
    Signed-off-by: Rusty Russell <rusty@au.ibm.com>

--- asm/asm-offsets.c
+++ asm/asm-offsets.c
@@ -7,7 +7,7 @@
 #include <stack.h>
 
 #define DEFINE(sym, val) \
-        asm volatile("\n->" #sym " %0 " #val : : "i" (val))
+        asm volatile("\n#define " #sym " %0 /* " #val " */" : : "i" (val))
 
 #define OFFSET(sym, str, mem) \
 	DEFINE(sym, offsetof(struct str, mem))
