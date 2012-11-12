098c4764637779c0e10a002e1d950b49a0055303
Rusty Russell <rusty@rustcorp.com.au>
2012-11-12 17:46:48 +1030

    cpus: create our own device tree
    We generate in-memory device tree nodes for the cpus as we discover
    them, rather than post-generating them.  We still keep the
    cpu_threads[] array, since that's accessed via asm and also has
    entries for threads (only primary threads aka cpus get a device node).
    
    We create our own root for the moment, but this will eventually be
    the global device-tree.
    
    Signed-off-by: Rusty Russell <rusty@au.ibm.com>

--- hdata/cpu.c
+++ hdata/cpu.c
@@ -5,6 +5,7 @@
 #include <device_tree.h>
 #include <opal.h>
 #include <ccan/str/str.h>
+#include <device.h>
 
 #define for_each_paca(p)						\
 	for (p = spira.ntuples.paca.addr;				\
@@ -83,12 +84,110 @@ static struct cpu_thread *populate_cpu_thread(const struct HDIF_cpu_id *id,
 	return t;
 }
 
+static struct dt_node *add_cpu_node(struct dt_node *cpus,
+				 const struct HDIF_common_hdr *paca,
+				 const struct HDIF_cpu_id *id)
+{
+	static const uint32_t p7_sps[] = {
+		0x0c, 0x000, 1, 0x0c, 0x0000,
+		0x18, 0x100, 1,	0x18, 0x0000,
+		0x14, 0x111, 1, 0x14, 0x0002,
+		0x22, 0x120, 1, 0x22, 0x0003,
+	};
+	static const uint32_t p7_pss[] = {
+		0x1c, 0x28, 0xffffffff, 0xffffffff
+	};
+	const struct HDIF_cpu_timebase *timebase;
+	const struct HDIF_cpu_cache *cache;
+	u32 no, size;
+	char name[sizeof("PowerPC,POWER7@") + STR_MAX_CHARS(no)];
+	struct dt_node *cpu;
+
+	/* We use the process_interrupt_line as the res id */
+	no = id->process_interrupt_line;
+
+	printf("CPU[%i]: PIR=%i RES=%i %s %s(%u threads)\n",
+	       paca_index(paca), id->pir, no,
+	       id->verify_exists_flags & CPU_ID_PACA_RESERVED
+	       ? "**RESERVED**" : cpu_state(id->verify_exists_flags),
+	       id->verify_exists_flags & CPU_ID_SECONDARY_THREAD
+	       ? "[secondary] " : 
+	       (id->pir == boot_cpu->pir ? "[boot] " : ""),
+	       ((id->verify_exists_flags
+		 & CPU_ID_NUM_SECONDARY_THREAD_MASK)
+		>> CPU_ID_NUM_SECONDARY_THREAD_SHIFT) + 1);
+
+	timebase = HDIF_get_idata(paca, 3, &size);
+	if (!timebase || size < sizeof(*timebase)) {
+		prerror("CPU[%i]: bad timebase size %u @ %p\n",
+			paca_index(paca), size, timebase);
+		return NULL;
+	}
+
+	cache = HDIF_get_idata(paca, 4, &size);
+	if (!cache || size < sizeof(*cache)) {
+		prerror("CPU[%i]: bad cache size %u @ %p\n",
+			paca_index(paca), size, cache);
+		return NULL;
+	}
+
+	printf("    Cache: I=%u D=%u/%u/%u/%u\n",
+	       cache->icache_size_kb,
+	       cache->l1_dcache_size_kb,
+	       cache->l2_dcache_size_kb,
+	       cache->l3_dcache_size_kb,
+	       cache->l35_dcache_size_kb);
+
+	/* FIXME: Don't hardcode this! */
+	sprintf(name, "PowerPC,POWER7@%u", no);
+	cpu = dt_new(cpus, name);
+	*strchr(name, '@') = '\0';
+	dt_add_property_string(cpu, "name", name);
+	dt_add_property_string(cpu, "device_type", "cpu");
+	dt_add_property_string(cpu, "status", "okay");
+	dt_add_property_cell(cpu, "reg", no);
+	dt_add_property(cpu, "ibm,segment-page-sizes", p7_sps, sizeof(p7_sps));
+	dt_add_property(cpu, "ibm,processor-segment-sizes",
+			p7_pss, sizeof(p7_pss));
+	/* XXX FIXME: Don't hardcode... */
+	dt_add_property_cell(cpu, "ibm,ppc-interrupt-server#s",
+			  no, no + 1, no + 2, no + 3);
+	dt_add_property_cell(cpu, "ibm,slb-size", 0x20);
+	dt_add_property_cell(cpu, "ibm,vmx", 0x2);
+
+	dt_add_property_cell(cpu, "d-cache-block-size", cache->dcache_block_size);
+	dt_add_property_cell(cpu, "i-cache-block-size", cache->icache_block_size);
+	dt_add_property_cell(cpu, "d-cache-size", cache->l1_dcache_size_kb*1024);
+	dt_add_property_cell(cpu, "i-cache-size", cache->icache_size_kb*1024);
+
+	if (cache->icache_line_size != cache->icache_block_size)
+		dt_add_property_cell(cpu, "i-cache-line-size",
+				  cache->icache_line_size);
+	if (cache->l1_dcache_line_size != cache->dcache_block_size)
+		dt_add_property_cell(cpu, "d-cache-line-size",
+				  cache->l1_dcache_line_size);
+	dt_add_property_cell(cpu, "clock-frequency",
+			  timebase->actual_clock_speed * 1000000);
+
+	/* FIXME: Hardcoding is bad. */
+	dt_add_property_cell(cpu, "timebase-frequency", 512000000);
+	return cpu;
+}
+
+static struct dt_node *dt_root;
+
 void early_init_boot_cpu_thread(void)
 {
 	const struct HDIF_common_hdr *paca = spira.ntuples.paca.addr;
 	const struct HDIF_cpu_id *id = NULL;
 	uint32_t boot_pir = mfspr(SPR_PIR);
 	struct cpu_thread *t;
+	struct dt_node *cpus, *cpu;
+
+	dt_root = dt_new_root("cpus-root");
+	cpus = dt_new(dt_root, "cpus");
+	dt_add_property_cell(cpus, "#address-cells", 2);
+	dt_add_property_cell(cpus, "#size-cells", 1);
 
 	if (boot_pir > SPR_PIR_MASK) {
 		prerror("Invalid boot pir %u\n", boot_pir);
@@ -130,6 +229,10 @@ void early_init_boot_cpu_thread(void)
 		abort();
 	}
 
+	cpu = add_cpu_node(cpus, paca, id);
+	if (!cpu)
+		abort();
+
 	t = populate_cpu_thread(id, paca_index(paca));
 	if (!t)
 		abort();
@@ -145,10 +248,24 @@ void early_init_boot_cpu_thread(void)
 	__this_cpu = boot_cpu = t;
 }
 
+static struct dt_node *find_cpus(void)
+{
+	struct dt_node *cpus;
+
+	/* Find our cpus node */
+	for (cpus = dt_first(dt_root); cpus; cpus = dt_next(dt_root, cpus)) {
+		if (streq(cpus->name, "cpus"))
+			break;
+	}
+	assert(cpus);
+	return cpus;
+}
+
 static bool __cpu_parse(void)
 {
 	const struct HDIF_common_hdr *paca;
 	uint32_t boot_pir = mfspr(SPR_PIR);
+	struct dt_node *cpus = find_cpus();
 
 	paca = spira.ntuples.paca.addr;
 	if (!HDIF_check(paca, "SPPACA")) {
@@ -192,6 +309,17 @@ static bool __cpu_parse(void)
 
 		if (!populate_cpu_thread(id, paca_index(paca)))
 			return false;
+
+		/* Only cpus we found. */
+		if (cpu_threads[id->pir].state != cpu_state_present)
+			continue;
+
+		/* Secondary threads don't get their own node. */
+		if (id->verify_exists_flags & CPU_ID_SECONDARY_THREAD)
+			continue;
+
+		if (!add_cpu_node(cpus, paca, id))
+			return false;
 	}
 	return true;
 }	
@@ -206,115 +334,20 @@ void cpu_parse(void)
 
 bool add_cpu_nodes(void)
 {
-	const struct HDIF_common_hdr *paca;
-	static const uint32_t p7_sps[] = {
-		0x0c, 0x000, 1, 0x0c, 0x0000,
-		0x18, 0x100, 1,	0x18, 0x0000,
-		0x14, 0x111, 1, 0x14, 0x0002,
-		0x22, 0x120, 1, 0x22, 0x0003,
-	};
-	static const uint32_t p7_pss[] = {
-		0x1c, 0x28, 0xffffffff, 0xffffffff
-	};
+	struct dt_node *cpus, *i;
+	struct dt_property *p;
 
-	dt_begin_node("cpus");
-	dt_property_cell("#address-cells", 2);
-	dt_property_cell("#size-cells", 1);
+	cpus = find_cpus();
 
-	for_each_paca(paca) {
-		char name[sizeof("PowerPC,POWER7@") + STR_MAX_CHARS(u32)];
-		const struct HDIF_cpu_id *id;
-		const struct HDIF_cpu_timebase *timebase;
-		const struct HDIF_cpu_cache *cache;
-		u32 size, no;
+	dt_begin_node(cpus->name);
+	list_for_each(&cpus->properties, p, list)
+		dt_property(p->name, p->prop, p->len);
 
-		id = HDIF_get_idata(paca, 2, &size);
-		if (!id || size < SPIRA_CPU_ID_MIN_SIZE) {
-			prerror("CPU[%i]: bad id size %u @ %p\n",
-				paca_index(paca), size, id);
-			return false;
-		}
-
-		/* Only cpus we brought up OK. */
-		if (id->pir > cpu_max_pir ||
-		    cpu_threads[id->pir].state != cpu_state_active)
-			continue;
-
-		/* We use the process_interrupt_line as the res id */
-		no = id->process_interrupt_line;
-
-		printf("CPU[%i]: PIR=%i RES=%i %s %s(%u threads)\n",
-		       paca_index(paca), id->pir, no,
-		       id->verify_exists_flags & CPU_ID_PACA_RESERVED
-		       ? "**RESERVED**" : cpu_state(id->verify_exists_flags),
-		       id->verify_exists_flags & CPU_ID_SECONDARY_THREAD
-		       ? "[secondary] " : 
-		       (id->pir == boot_cpu->pir ? "[boot] " : ""),
-		       ((id->verify_exists_flags
-			 & CPU_ID_NUM_SECONDARY_THREAD_MASK)
-			>> CPU_ID_NUM_SECONDARY_THREAD_SHIFT) + 1);
-
-		if (id->verify_exists_flags & CPU_ID_SECONDARY_THREAD)
-			continue;
-
-		timebase = HDIF_get_idata(paca, 3, &size);
-		if (!timebase || size < sizeof(*timebase)) {
-			prerror("CPU[%i]: bad timebase size %u @ %p\n",
-				paca_index(paca), size, timebase);
-			return false;
-		}
-
-		cache = HDIF_get_idata(paca, 4, &size);
-		if (!cache || size < sizeof(*cache)) {
-			prerror("CPU[%i]: bad cache size %u @ %p\n",
-				paca_index(paca), size, cache);
-			return false;
-		}
-
-		printf("    Cache: I=%u D=%u/%u/%u/%u\n",
-		       cache->icache_size_kb,
-		       cache->l1_dcache_size_kb,
-		       cache->l2_dcache_size_kb,
-		       cache->l3_dcache_size_kb,
-		       cache->l35_dcache_size_kb);
-
-		/* FIXME: Don't hardcode this! */
-		sprintf(name, "PowerPC,POWER7@%u", no);
-		dt_begin_node(name);
-		*strchr(name, '@') = '\0';
-		dt_property_string("device_type", "cpu");
-		dt_property_string("status", "okay");
-		dt_property_cell("reg", no);
-		dt_property("ibm,segment-page-sizes", p7_sps, sizeof(p7_sps));
-		dt_property("ibm,processor-segment-sizes", p7_pss,
-			    sizeof(p7_pss));
-		/* XXX FIXME: Don't hardcode... */
-		dt_property_cells("ibm,ppc-interrupt-server#s", 4,
-				  no, no + 1, no + 2, no + 3);
-		dt_property_cell("ibm,slb-size", 0x20);
-		dt_property_cell("ibm,vmx", 0x2);
-
-		dt_property_cell("d-cache-block-size",
-				 cache->dcache_block_size);
-		dt_property_cell("i-cache-block-size",
-				 cache->icache_block_size);
-		dt_property_cell("d-cache-size",
-				 cache->l1_dcache_size_kb*1024);
-		dt_property_cell("i-cache-size",
-				 cache->icache_size_kb*1024);
-
-		if (cache->icache_line_size != cache->icache_block_size)
-			dt_property_cell("i-cache-line-size",
-					 cache->icache_line_size);
-		if (cache->l1_dcache_line_size !=
-		    cache->dcache_block_size)
-			dt_property_cell("d-cache-line-size",
-					 cache->l1_dcache_line_size);
-		dt_property_cell("clock-frequency",
-				 timebase->actual_clock_speed * 1000000);
-
-		/* FIXME: Hardcoding is bad. */
-		dt_property_cell("timebase-frequency", 512000000);
+	/* This only works because the tree under cpus is flat. */
+	for (i = dt_first(cpus); i; i = dt_next(cpus, i)) {
+		dt_begin_node(i->name);
+		list_for_each(&i->properties, p, list)
+			dt_property(p->name, p->prop, p->len);
 		dt_end_node();
 	}
 	dt_end_node();
