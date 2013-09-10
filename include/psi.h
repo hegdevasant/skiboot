e2a4285c982a8f8cbecff8924e863cc64d8af4d2
Ananth N Mavinakayanahalli <ananth@in.ibm.com>
2013-09-10 16:30:11 +0530

    PSI: Fix interrupt bit mappings for PSI host bridge interrupts
    There is a discrepancy between P7 Book4 and SCOM docs about which bits
    are the PSI/FSP interrupts in the PSI Host Bridge Control register.
    Based on BenH and the FSP team's inputs and from tests on hardware,
    the SCOM docs are right. Fix code appropriately.
    
    This really doesn't affect P8. Tested on P7.
    
    Signed-off-by: Ananth N Mavinakayanahalli <ananth@in.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- include/psi.h
+++ include/psi.h
@@ -50,8 +50,10 @@
 #define   PSIHB_CR_PSI_LINK_ENABLE	PPC_BIT(5)
 #define   PSIHB_CR_FSP_RESET		PPC_BIT(6)
 #define   PSIHB_CR_PSIHB_RESET		PPC_BIT(7)
-#define   PSIHB_CR_PSI_IRQ		PPC_BIT(16)	/* PSIHB interrupt */
-#define   PSIHB_CR_FSP_IRQ		PPC_BIT(17)	/* FSP interrupt */
+#define   PSIHB_CR_P7_PSI_IRQ		PPC_BIT(12)	/* PSIHB interrupt */
+#define   PSIHB_CR_P7_FSP_IRQ		PPC_BIT(13)	/* FSP interrupt */
+#define   PSIHB_CR_P8_PSI_IRQ		PPC_BIT(16)	/* PSIHB interrupt */
+#define   PSIHB_CR_P8_FSP_IRQ		PPC_BIT(17)	/* FSP interrupt */
 
 /* PSI Status / Error Mask Register */
 #define PSIHB_SEMR			0x28
