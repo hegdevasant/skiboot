hegdevasant/skiboot

commit a84596cf97c1d0f4a4cd45314ab1007894ac92d0

trace: allocate node-local trace buffers once we have mem layout.
We switch over from the static buffer during early skiboot.

Signed-off-by: Rusty Russell <rusty@rustcorp.com.au>

core/trace.c
@@ -34,17 +34,6 @@ static size_t tracebuf_extra(void)
 	return TBUF_SZ + MAX_SIZE;
 }
 
-struct trace_info *trace_new_info(void)
-{
-	struct trace_info *ti;
-
-	ti = zalloc(sizeof(*ti) + tracebuf_extra());
-	init_lock(&ti->lock);
-	ti->tb.mask = TBUF_SZ - 1;
-	ti->tb.max_size = MAX_SIZE;
-	return ti;
-}
-
 /* To avoid bloating each entry, repeats are actually specific entries.
  * tb->last points to the last (non-repeat) entry. */
 static bool handle_repeat(struct tracebuf *tb, const union trace *trace)
@@ -133,6 +122,31 @@ void trace_add(union trace *trace)
 		ti->tb.end += trace->hdr.len_div_8 * 8;
 	}
 	unlock(&ti->lock);
+}
+
+/* Allocate trace buffers once we know memory topology */
+void init_trace_buffers(void)
+{
+	struct cpu_thread *t;
+
+	/* Allocate a trace buffer for each primary cpu. */
+	for_each_cpu(t) {
+		if (t->is_secondary)
+			continue;
+		
+		t->trace = local_alloc(t, sizeof(*t->trace) + tracebuf_extra(),
+				       __alignof__(*t->trace));
+		init_lock(&t->trace->lock);
+		t->trace->tb.mask = TBUF_SZ - 1;
+		t->trace->tb.max_size = MAX_SIZE;
+	}
+
+	/* And copy those to the secondaries. */
+	for_each_cpu(t) {
+		if (!t->is_secondary)
+			continue;
+		t->trace = t->primary->trace;
+	}
 }
 
 void trace_add_node(void)

core/cpu.c
@@ -354,7 +354,6 @@ void init_all_cpus(void)
 		enum cpu_thread_state state;
 		const struct dt_property *p;
 		struct cpu_thread *t, *pt;
-		struct trace_info *ti;
 
 		/* Skip cache nodes */
 		if (strcmp(dt_prop_get(cpu, "device_type"), "cpu"))
@@ -383,9 +382,9 @@ void init_all_cpus(void)
 		t = pt = &cpu_stacks[pir].cpu;
 		if (t != boot_cpu) {
 			init_cpu_thread(t, state, pir);
-			t->trace = trace_new_info();
+			/* Each cpu gets its own later in init_trace_buffers */
+			t->trace = boot_cpu->trace;
 		}
-		ti = t->trace;
 		t->server_no = server_no;
 		t->primary = t;
 		t->node = cpu;
@@ -406,7 +405,7 @@ void init_all_cpus(void)
 			printf("CPU:   secondary thread %d found\n", thread);
 			t = &cpu_stacks[pir + thread].cpu;
 			init_cpu_thread(t, state, pir + thread);
-			t->trace = ti;
+			t->trace = boot_cpu->trace;
 			t->server_no = ((u32 *)p->prop)[thread];
 			t->is_secondary = true;
 			t->primary = pt;

core/init.c
@@ -434,6 +434,9 @@ void main_cpu_entry(const void *fdt, u32 master_cpu)
 	/* Mark out memory areas. */
 	mem_region_init();
 
+	/* Separate out trace buffers now. */
+	init_trace_buffers();
+
 	/* Initialize XSCOM */
 	xscom_init();
 

include/trace.h
@@ -25,7 +25,8 @@ struct trace_info {
 	struct tracebuf tb;
 };
 
-struct trace_info *trace_new_info(void);
+/* Allocate trace buffers once we know memory topology */
+void init_trace_buffers(void);
 
 /* This will fill in timestamp and cpu; you must do type and len. */
 void trace_add(union trace *trace);

commit 997d8ecd00dc7cfc49b3c797f168ba4a920983b5

trace: zero out buffer to start.
Broken in a84596cf97c1d; we switched zalloc to local_alloc.

Signed-off-by: Rusty Russell <rusty@au.ibm.com>

core/trace.c
@@ -138,6 +138,7 @@ void init_trace_buffers(void)
 		
 		t->trace = local_alloc(t, sizeof(*t->trace) + tracebuf_extra(),
 				       __alignof__(*t->trace));
+		memset(t->trace, 0, sizeof(*t->trace) + tracebuf_extra());
 		init_lock(&t->trace->lock);
 		t->trace->tb.mask = TBUF_SZ - 1;
 		t->trace->tb.max_size = MAX_SIZE;