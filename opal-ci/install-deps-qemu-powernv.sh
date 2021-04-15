94eeeaa4184454f9d64f68ed07a4d208b8d9ce16
Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
2021-04-15 16:09:05 +0530

    qemu fix

--- opal-ci/install-deps-qemu-powernv.sh
+++ opal-ci/install-deps-qemu-powernv.sh
@@ -2,4 +2,4 @@
 set -e
 sudo apt-get -y install eatmydata
 sudo eatmydata apt-get -y install gcc python g++ pkg-config \
-libz-dev libglib2.0-dev libpixman-1-dev libfdt-dev git
+libz-dev libglib2.0-dev libpixman-1-dev libfdt-dev git ninja-build
