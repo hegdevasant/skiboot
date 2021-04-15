d794ff30db8eb54440aec5805cd44858aaed6163
Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
2021-04-15 17:42:59 +0530

    doc test

--- opal-ci/build-docs.sh
+++ opal-ci/build-docs.sh
@@ -8,7 +8,7 @@ MAKE_J=$(grep -c processor /proc/cpuinfo)
 export CROSS="ccache powerpc64-linux-gnu-"
 
 (cd opal-ci; ./build-qemu-powernv.sh)
-export QEMU_BIN=$(pwd)/opal-ci/qemu/ppc64-softmmu/qemu-system-ppc64
+export QEMU_BIN=$(pwd)/opal-ci/qemu/build/qemu-system-ppc64
 
 make -j${MAKE_J} SKIBOOT_GCOV=1 coverage-report
 make SKIBOOT_GCOV=1 boot-coverage-report
